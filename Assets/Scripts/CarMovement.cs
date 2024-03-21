25b952715ef0769196a21a7e605dc82b65a39a12
L. Cooper <104042225+thanksfortheride136@users.noreply.github.com>
2024-03-21 13:27:11 -0400

    init commit teacher finish version
    Finished version on main

--- Assets/Scripts/CarMovement.cs
+++ Assets/Scripts/CarMovement.cs
@@ -1,10 +1,4 @@
-﻿/* Unity Game Programming
- * Thunder Road Project
- * Thunder Road Author
- * Project Date
- */
-
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using System.Collections;
@@ -39,33 +33,14 @@ public class CarMovement : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		// only process movement and new time if the
-		// game is not yet over
-		// ??replace this line with an if() statement ?? 
+		if(!gameOver)
 		{
-			// Move the car in current direction at
-			// the current speed.  Translation in the "X"
-			// direction will move an object forward at
-			// it's current rotation angle. Adjust the 
-			// 0.02F scale factor to move faster or slower.
 			transform.Translate(speed * Time.deltaTime, 0, 0);
-
-			// Get steering left/right input.  Adjust
-			// the 100.0F scale factor to turn faster
-			// or slower
 			float rotation = Input.GetAxis("Horizontal") * 100.0F;
-
-			// rotate car left or right according to current input
-			// (rotation will be 0 if no input)
 			transform.Rotate(0, 0, -rotation);
-
-			// add time since the last frame to our measure
-			// of overall elapsed time
 			elapsedTime += Time.deltaTime;
-
-			// update the timeText message, converting float to int
 			timeText.text = "Time: " + (int)elapsedTime;
-		}
+		}	
 	}
 
 	void OnTriggerEnter2D(Collider2D otherObject)
@@ -78,7 +53,17 @@ public class CarMovement : MonoBehaviour
 		// object's name
 		switch (otherName) 
 		{
-		   // ?? complete all cases of this switch statement ??
+			case "finishLine":
+				gameOver = true;
+				break;
+			case "speedBoost1":
+			case "speedBoost2":
+			case "speedBoost3":
+			case "speedBoost4":
+				speed += 1.0f;
+				speedText.text = "Speed: " + speed;
+				break;
+
 		}
 	}
 
@@ -88,10 +73,18 @@ public class CarMovement : MonoBehaviour
 
 		// on any collision with an obstacle object,
 		// increase damage by 1 and update the damageText display
-		damage++;
-		damageText.text = "Damage: " + damage;
+		switch (otherObject.gameObject.name)
+		{
+			case "rock1":
+			damage++;
+			damageText.text = "Damage: " + damage;
+			break;
+		}
 
-		// ?? add an if() statement and logic to handle 
-		// "Wrecked!" game over condition ??
+		if (damage >= 10)
+		{
+			gameOver = true;
+			gameOverText.text = "Wrecked!";
+		}
 	}
 }
